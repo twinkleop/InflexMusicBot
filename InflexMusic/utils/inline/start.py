efe488980c125ff5d76941a1f0d79ef10653a99a
Team Inflex <124878653+TeamInflex@users.noreply.github.com>
2023-02-09 22:15:06 +0530

    Update start.py

--- InflexMusic/utils/inline/start.py
+++ InflexMusic/utils/inline/start.py
@@ -128,7 +128,4 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                     ),
                 ]
             )
-    buttons.append(
-        [InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG")]
-    )
     return buttons
